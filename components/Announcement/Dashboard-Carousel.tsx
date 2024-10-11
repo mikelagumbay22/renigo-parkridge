193a6720d78f644e443238b3d868e7418acbcf88
mikelagumbay22 <58532174+mikelagumbay22@users.noreply.github.com>
2024-10-11 02:40:11 -0400

    feat: add annoucement carousel

--- components/Announcement/Dashboard-Carousel.tsx
+++ components/Announcement/Dashboard-Carousel.tsx
@@ -15,6 +15,7 @@ const CarouselItem: React.FC<CarouselItemProps> = ({ title, imageSrc, dateTime }
       width={213}
       height={325}
       className="rounded-lg object-cover"
+      style={{ objectFit: "cover", width: "213px", height: "325px" }}
     />
     <div className="mt-4 text-center">
       <h3 className="text-lg font-semibold">{title}</h3>
@@ -26,33 +27,33 @@ const CarouselItem: React.FC<CarouselItemProps> = ({ title, imageSrc, dateTime }
 export default function Carousel() {
   const items = [
     {
-      title: "Sample 1",
-      imageSrc: "/images/sample1.jpg",
+      title: "Gate Close - Renovation",
+      imageSrc: "https://toprealty.com.ph/wp-content/uploads/2022/06/parkridge.jpg",
       dateTime: "2024-10-01 10:30 AM",
     },
     {
-      title: "Sample 2",
-      imageSrc: "/images/sample2.jpg",
+      title: "Vehicle Accident - Sumulong",
+      imageSrc: "https://d78e7d51k2tkt.cloudfront.net/bc52d64e649b4e23ab1b0ef43fbce637.webp",
       dateTime: "2024-10-01 11:45 AM",
     },
     {
-      title: "Sample 3",
-      imageSrc: "/images/sample3.jpg",
+      title: "Heavy Traffic in Marcos Highway",
+      imageSrc: "https://pbs.twimg.com/media/CdgEFDrUYAA_8DT.jpg",
       dateTime: "2024-10-01 12:00 PM",
     },
     {
-      title: "Sample 4",
-      imageSrc: "/images/sample4.jpg",
+      title: "Simbang Gabi Schedule",
+      imageSrc: "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEhVtVY_kv8i_APLNsyJfBy9OaDiniygaq1AF3iC15Kollg9sV9K3X574EGMgPR3Edtirwz9N3el04S2y4o-Vfixaswq7A39b4Ob-CPwMJB6sqgLbwUAJOPzVw-XgIkX-0JwD7S7p_SBJm-JcFnQzbLiGC5FQVVdhsBNulV65LcP7Gzzz-WVM-NOmB5XQA/s2048/San%20Pedro%20Calungsod%20Parish%20-%20Sta.%20Cruz,%20Antipolo%20City,%20Rizal.jpg",
       dateTime: "2024-10-02 09:15 AM",
     },
     {
-      title: "Sample 5",
-      imageSrc: "/images/sample5.jpg",
+      title: "3x3 Basket Ball Tournament",
+      imageSrc: "https://marketplace.canva.com/EAFqmAGGZbE/1/0/283w/canva-orange-and-white-bold-basketball-tournament-document-a4-vkPJPFdeHY4.jpg",
       dateTime: "2024-10-02 10:45 AM",
     },
     {
-      title: "Sample 6",
-      imageSrc: "/images/sample6.jpg",
+      title: "ParkRidge Gym Opening",
+      imageSrc: "https://marketplace.canva.com/EAE6jwsJsL8/1/0/1131w/canva-black-minimalist-gym-center-promo-poster-btQabfai1Wo.jpg",
       dateTime: "2024-10-03 01:00 PM",
     },
   ];
--- next.config.js
+++ next.config.js
@@ -1,4 +1,29 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'toprealty.com.ph',
+      },
+      {
+        protocol: 'https',
+        hostname: 'd78e7d51k2tkt.cloudfront.net',
+      },
+      {
+        protocol: 'https',
+        hostname: 'pbs.twimg.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'blogger.googleusercontent.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'marketplace.canva.com',
+      },
+    ],
+  },
+};
 
 module.exports = nextConfig;
